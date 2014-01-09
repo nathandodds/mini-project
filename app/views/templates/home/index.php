6f561fe7839b30e1983d46a8657a5a6ddc4e1180
nathandodds <nathan@stormcreative.co.uk>
2014-01-09 16:02:42 +0000

    desktop layout main/aside/content update

--- app/views/templates/home/index.php
+++ app/views/templates/home/index.php
@@ -13,51 +13,52 @@
 	<article class="kwc-text">
 		<p>You can find more sexual health information, as well as advice and guidance on a range of other health-related topics, by visiting the <a href="">KWC website.</a></p>
 	</article>
-	<section>
-		<article class="header2">
-		 	<h2>Real women, real stories</h2>
-		 	<p>Learn how other women found the right contraception for them</p>
-		</article>
-		<article class="story">
-			<div class="big-story">
-			<h3>The implant:</h3>
-			<p>Having the implant allowed me to take control of my life, putting my career and social life first without having to worry about pregnancy.</p>
-			<div class="story__person">
-				<p class="stories-names">Rebecca, 27</p>
-				<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-1.png" /></span>
-			</div>
-			</div>
-		</article>
-		<article class="story">
-			<div class="small-story">
+	<article class="header2">
+	 	<h2>Real women, real stories</h2>
+	 	<p>Learn how other women found the right contraception for them</p>
+	</article>
+	<section class="section">
+		<div class="big-story">
+			<article class="story">
+				<h3>The implant:</h3>
+				<p>Having the implant allowed me to take control of my life, putting my career and social life first without having to worry about pregnancy.</p>
+				<div class="story__person">
+					<p class="stories-names">Rebecca, 27</p>
+					<span class="story__img--rebecca"><img src="<?php echo DIRECTORY;?>assets/images/story-img-1.png" /></span>
+					<span class="story__desktop-img"><img src="<?php echo DIRECTORY;?>assets/images/story-images-1-desktop.png" /></span>
+				</div>
+			</article>
+		</div>
+		<div class="small-story">
+			<article class="story">
 				<h3>The injection:</h3>
 				<p>I’m starting to settle down now and so the contraceptive injection is great for me...</p>
 				<div class="story__person">
 					<p class="stories-names">Sarah, 31</p>
 					<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-2.png" /></span>
 				</div>
-			</div>
-		</article>
-		<article class="story">
-			<div class="small-story">
+			</article>
+		</div>
+		<div class="small-story">
+			<article class="story">
 				<h3>The combined pill:</h3>
 				<p>I’m young fit and healthy and want kids very soon – but not just yet. I’m not a fan of needles so the injection is not an option...  </p>
 				<div class="story__person">
 					<p class="stories-names">Lucy, 24</p>
 					<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-3.png" /></span>
 				</div>
-			</div>
-		</article>
-		<article class="story">
-			<div class="small-story">
+			</article>
+		</div>
+		<div class="small-story">
+			<article class="story">
 				<h3>POP pill:</h3>
 				<p>I believe in enjoying life and doing what I want, when I want. So, when it comes to contraception the POP pill is the one for me...</p>
 				<div class="story__person">
 					<p class="stories-names">Tracy, 36</p>
 					<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-4.png" /></span>
 				</div>
-			</div>
-		</article>
+			</article>
+		</div>
 	</section>
 	<?php include "assets/includes/aside.php"; ?>
 </div>
