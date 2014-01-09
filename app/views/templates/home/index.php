455f78125e9723750cd15acd72d94dcb625844e1
nathandodds <nathan@stormcreative.co.uk>
2014-01-09 13:15:01 +0000

    desktop layout update

--- app/views/templates/home/index.php
+++ app/views/templates/home/index.php
@@ -8,7 +8,7 @@
 	<article class="main-text-top">
 		<h1>Contraception that’s right for you</h1>
 		<p>With so many different female contraceptive methods out there, how do you choose the best one for you?</p>
-		<a href="">Take the test here to help you decide</a>
+		<a href="">Discover your contraceptive options</a>
 	</article>
 	<article class="kwc-text">
 		<p>You can find more sexual health information, as well as advice and guidance on a range of other health-related topics, by visiting the <a href="">KWC website.</a></p>
@@ -19,35 +19,43 @@
 		 	<p>Learn how other women found the right contraception for them</p>
 		</article>
 		<article class="story">
-			<h3>Lorem ipsum dolor sit ame.</h3>
-			<p>Maecenas tincidunt iaculis hendrerit vestibulum mattis urna et leo faucibus blandit. Aenean tempor congue leo eget ornare.</p>
+			<div class="big-story">
+			<h3>The implant:</h3>
+			<p>Having the implant allowed me to take control of my life, putting my career and social life first without having to worry about pregnancy.</p>
 			<div class="story__person">
 				<p class="stories-names">Rebecca, 27</p>
 				<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-1.png" /></span>
 			</div>
-		</article>
-		<article class="story">
-			<h3>Lorem ipsum dolor sit ame.</h3>
-			<p>Maecenas tincidunt iaculis hendrerit vestibulum mattis urna et leo faucibus blandit. Aenean tempor congue leo eget ornare.</p>
-			<div class="story__person">
-				<p class="stories-names">Rebecca, 27</p>
-				<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-2.png" /></span>
 			</div>
 		</article>
 		<article class="story">
-			<h3>Lorem ipsum dolor sit ame.</h3>
-			<p>Maecenas tincidunt iaculis hendrerit vestibulum mattis urna et leo faucibus blandit. Aenean tempor congue leo eget ornare.</p>
-			<div class="story__person">
-				<p class="stories-names">Rebecca, 27</p>
-				<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-3.png" /></span>
+			<div class="small-story">
+				<h3>The injection:</h3>
+				<p>I’m starting to settle down now and so the contraceptive injection is great for me...</p>
+				<div class="story__person">
+					<p class="stories-names">Sarah, 31</p>
+					<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-2.png" /></span>
+				</div>
 			</div>
 		</article>
 		<article class="story">
-			<h3>Lorem ipsum dolor sit ame.</h3>
-			<p>Maecenas tincidunt iaculis hendrerit vestibulum mattis urna et leo faucibus blandit. Aenean tempor congue leo eget ornare.</p>
-			<div class="story__person">
-				<p class="stories-names">Rebecca, 27</p>
-				<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-4.png" /></span>
+			<div class="small-story">
+				<h3>The combined pill:</h3>
+				<p>I’m young fit and healthy and want kids very soon – but not just yet. I’m not a fan of needles so the injection is not an option...  </p>
+				<div class="story__person">
+					<p class="stories-names">Lucy, 24</p>
+					<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-3.png" /></span>
+				</div>
+			</div>
+		</article>
+		<article class="story">
+			<div class="small-story">
+				<h3>POP pill:</h3>
+				<p>I believe in enjoying life and doing what I want, when I want. So, when it comes to contraception the POP pill is the one for me...</p>
+				<div class="story__person">
+					<p class="stories-names">Tracy, 36</p>
+					<span class="story__img"><img src="<?php echo DIRECTORY;?>assets/images/story-img-4.png" /></span>
+				</div>
 			</div>
 		</article>
 	</section>
