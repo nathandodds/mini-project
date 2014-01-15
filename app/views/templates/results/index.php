56955b5173454c42b816ac10899de27a695e255c
nathandodds <nathan@stormcreative.co.uk>
2014-01-15 17:57:48 +0000

    mobile and desktop changes- images/aside

--- app/views/templates/results/index.php
+++ app/views/templates/results/index.php
@@ -8,56 +8,59 @@
 		<li><a href="#">Review my answers</a></li>
 	</ul>
 </header>
-<div class="results-content">
-	<section class="results-section">
-		<article class="results-article">
-			<div class="contra-inject">
-				<img src="<?php echo DIRECTORY; ?>assets/images/contra-inject-mobile_r2_c1.png" class="results-content__image"/>
-				<h2>Contraceptive Injection</h2>
-				<p>Containing the hormone progestogen, the contraceptive injection lasts for either 8 or 12 weeks.</p>
-				<a href="#"> Find out more</a>
-			</div>
-		</article>
-		<article class="results-article">
-			<div class="implant">
-				<img src="<?php echo DIRECTORY; ?>assets/images/implants-mobile_r2_c1.png" class="results-content__image"/>
-				<h2>Implants</h2>
-				<p>A flexible tube full of progestogen, an implant can be effective for three years.</p>
-				<a href="#"> Find out more</a>
-			</div>
-		</article>
-		<article class="results-article">
-			<div class="ius">
-				<img src="<?php echo DIRECTORY; ?>assets/images/ius-mobile_r2_c1.png" class="results-content__image"/>
-				<h2>Intrauterine System (IUS)</h2>
-				<p>Working for up to five years, an IUS releases the hormone progestogen to help prevent pregnancy.</p>
-				<a href="#"> Find out more</a>
-			</div>
-		</article>
-		<article class="results-article">
-			<div class="iud">
-				<img src="<?php echo DIRECTORY; ?>assets/images/iud-mobile_r2_c1.png" class="results-content__image"/>
-				<h2>Intrauterine Device (IUD)</h2>
-				<p>Long-lasting but reversible, IUDs are fitted by a trained doctor or nurse.</p>
-				<a href="#"> Find out more</a>
-			</div>
-		</article>
-		<article class="results-article">
-			<div class="coc">
-				<img src="<?php echo DIRECTORY; ?>assets/images/coc-mobile_r2_c1.png" class="results-content__image"/>
-				<h2>Combined Pill (COC)</h2>
-				<p>The combined pill contains both oestrogen and progestogen and helps prevent pregnancy.</p>
-				<a href="#"> Find out more</a>
-			</div>
-		</article>
+<div class="content">
+	<div class="results-content">
+		<ol class="results__list">
+			<li class="results-article">
+				<div class="contra-inject">
+					<img src="<?php echo DIRECTORY; ?>assets/images/retina/contra-inject-mobile_2x.png" class="results-content__image"/>
+					<h2>Contraceptive Injection</h2>
+					<p>Containing the hormone progestogen, the contraceptive injection lasts for either 8 or 12 weeks.</p>
+					<a href="#"> Find out more</a>
+				</div>
+			</li>
+			<li class="results-article">
+				<div class="implant">
+					<img src="<?php echo DIRECTORY; ?>assets/images/implants-mobile.png" class="results-content__image-mobile"/>
+					<h2>Implants</h2>
+					<p>A flexible tube full of progestogen, an implant can be effective for three years.</p>
+					<a href="#"> Find out more</a>
+				</div>
+			</li>
+			<li class="results-article">
+				<div class="ius">
+					<img src="<?php echo DIRECTORY; ?>assets/images/ius-mobile.png" class="results-content__image-mobile"/>
+					<h2>Intrauterine System (IUS)</h2>
+					<p>Working for up to five years, an IUS releases the hormone progestogen to help prevent pregnancy.</p>
+					<a href="#"> Find out more</a>
+				</div>
+			</li>
+			<li class="results-article">
+				<div class="iud">
+					<img src="<?php echo DIRECTORY; ?>assets/images/iud-mobile.png" class="results-content__image-mobile"/>
+					<h2>Intrauterine Device (IUD)</h2>
+					<p>Long-lasting but reversible, IUDs are fitted by a trained doctor or nurse.</p>
+					<a href="#"> Find out more</a>
+				</div>
+			</li>
+			<li class="results-article">
+				<div class="coc">
+					<img src="<?php echo DIRECTORY; ?>assets/images/coc-mobile.png" class="results-content__image-mobile"/>
+					<h2>Combined Pill (COC)</h2>
+					<p>The combined pill contains both oestrogen and progestogen and helps prevent pregnancy.</p>
+					<a href="#"> Find out more</a>
+				</div>
+			</li>
+		</ol>
 		<div class="aside__treatment--results">
 			<h3>Where can I get treatment?</h3>
 			<p class="aside__treatment--text">You can receive further advice on contraception by visiting your GP or contacting your local CaSH service, details of these can be found on the <a href="">NHS Choices website.</a></p>
 		</div>
-		<article class="kwc-text">
-			<p>You can find more sexual health information, as well as advice and guidance on a range of other health-related topics, by visiting the <a href="">KWC website.</a></p>
+		<article class="kwc-text-results">
+				<p>You can find more sexual health information, as well as advice and guidance on a range of other health-related topics, by visiting the <a href="">KWC website.</a></p>
 		</article>
-	</section>
+	</div>
 	<?php include "assets/includes/results_aside.php"; ?>
 </div>
+
 
--- assets/includes/results_aside.php
+++ assets/includes/results_aside.php
@@ -1,10 +1,29 @@
+<div class="results-aside">
 <aside class="aside">
 	<div class="aside__whatnow">
+	<div class="whatnow-border">
 		<h3>What to do now</h3>
 		<p class="aside__whatnow--text">Now you have your results, you should speak to your GP or visit your local CaSH service.</p>
-		<p class="whatnow-img"><a href="#">whatnow</a></p>
-		<p class="find-local"><a href= "#">Find your local service</a></p>
+		<p class="find-local" "whatnow-border"><a href= "#">Find your local service</a></p>
 	</div>
+	<div class="whatnow-border">
+		<p class="aside__whatnow--text">In order to access your results in the future, you can:</p>
+		<p class="save-results"><a href= "#"> Save my results </a></p>
+		<p class="text-results"><a href="#"> Text me my results </a></p>
+		<p class="email-results" "whatnow-border"><a href="#"> Email me my results </a></p>
+	</div>
+		<p class="aside__whatnow--text">Tell a friend about Contraception Advisor</p>
+		<p class="recommend-friend"><a href="#">Recommend a friend </a></p>
+	</div>
+	<div class="aside__rebecca">
+		<h3>The implant:</h3>
+		<p><span class="rebecca-text-pink">Having the implant allowed me to take control of my life, putting my career and social life first without having to worry about pregnancy.</span></p>
+		<p><span class="rebecca-text-black"> That’s not to say that I don’t want to get pregnant in the future, but only when I am good and ready. And that’s what is great about... </span></p>
+	<div class="aside__person">
+		<p class="aside-name">Rebecca, 27</p>
+		<span class="aside__img--rebecca"><img src="<?php echo DIRECTORY;?>assets/images/story-img-1.png" /></span>					
+	</div>
+</div>
 	<div class="aside__myth">
 		<h3>Mythbuster</h3>
 		<div class="myth-text">
@@ -13,3 +32,4 @@
 		</div>
 	</div>
 </aside>
+</div>
