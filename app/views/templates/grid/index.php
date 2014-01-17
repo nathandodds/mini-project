848aaf75f7f7af767182f12d35359e629e989edc
nathandodds <nathan@stormcreative.co.uk>
2014-01-17 16:54:53 +0000

    comments added

--- app/views/templates/grid/index.php
+++ app/views/templates/grid/index.php
@@ -22,7 +22,7 @@
 		<p>Review my answers</p>
 	</div>						
 <div class="grid grid-container"> <!--***MUST START WITH grid TO APPLY THE GRID TO THE PAGE***-->
-	<section class="grid__two-third">
+	<section class="grid__two-third"> <!--***THIS SECTION WILL BE 2/3 OF THESET GRID WIDTH***-->
 		<article class="results-content">
 			<h2>Contraceptive Injection</h2>
 			<p>Containing the hormone progestogen, the contraceptive injection lasts for either 8 or 12 weeks.</p>
@@ -56,7 +56,7 @@
 			<p>You can find more sexual health information, as well as advice and guidance on a range of other health-related topics, by visiting the <a href="#">KWC website.</a></p>
 		</article>
 	</section>
-	<aside class="grid__third results-aside">
+	<aside class="grid__third results-aside"> <!--***THIS ASIDE WILL BE 1/3 OF THE SET GRID WIDTH***-->
 		<article class="aside__whatnow">
 			<div class="whatnow-border">
 				<h3>What to do now</h3>
@@ -89,111 +89,3 @@
 	</aside>
 </div>
 </div>
-
-<!--	<article class="wtdn">
-			<h2>What to do now</h2>
-			<p>Now you have your results, you should speak to your GP or visit your local CaSH service. </p>
-			<ul>
-				<li><a href="#">Find your local service</a></li>
-			</ul>
-			<div class="what-now-border">
-				<p>In order to access your results in the future, you can :</p>
-				<ul>
-					<li><a href="#">Save my results</a></li>
-					<li><a href="#">Text me my results</a></li>
-					<li><a href="#">Email me results</a></li>
-				</ul>
-			</div>
-			<div class="what-now-border">
-				<h2>Tell a friend</h2>
-				<ul>
-					<li><a href="#"> Recommend a friend</a></li>
-				</ul>
-			</div>
-		</article>-->
-
-
-	<!--<div class="grid__third results-aside">
-			<div class="results">
-			<div class="aside__whatnow">
-			<div class="whatnow-border">
-				<h3>What to do now</h3>
-				<p class="aside__whatnow--text">Now you have your results, you should speak to your GP or visit your local CaSH service.</p>
-				<p class="find-local" "whatnow-border"><a href= "#">Find your local service</a></p>
-			</div>
-			<div class="whatnow-border">
-				<p class="aside__whatnow--text">In order to access your results in the future, you can:</p>
-				<p class="save-results"><a href= "#"> Save my results </a></p>
-				<p class="text-results"><a href="#"> Text me my results </a></p>
-				<p class="email-results" "whatnow-border"><a href="#"> Email me my results </a></p>
-			</div>
-			<span class="recommend-friend-desktop"><p>Tell a friend</p></span>
-			<span class="recommend-friend-mobile"><p class="aside__whatnow--text">Tell a friend about Contraception Advisor</p></span>
-			<p class="recommend-friend"><a href="#">Recommend a friend </a></p>
-			</div>
-			<div class="aside__rebecca">
-				<h3>The injection:</h3>
-				<p><span class="rebecca-text-pink">I’m starting to settle down now and so the contraceptive injection is great for me.</span></p>
-				<p><span class="rebecca-text-black">I’m not ready to have children just yet but I think I may want to start trying within the next year to eighteen months. It lasts for around three months so I know that as soon as I want to start trying for a baby, I won’t have to wait long to be in a position to try to   conceive. In the meantime, I can continue my sex life without worrying about protection or taking a pill everyday.</span></p>
-				<div class="aside__person">
-					<p class="aside-name">Rebecca, 27</p>
-					<span class="aside__img--rebecca"><img src="<?php echo DIRECTORY;?>assets/images/story-img-1.png" /></span>					
-				</div>
-			</div>
-			<div class="aside__myth">
-				<h3>Mythbuster</h3>
-			<div class="myth-text">
-				<p class="question1"><span class="q-a">Q</span><span class="q-a-result">The coil falls out during sex.</span></p>
-				<p><span class="q-a">A</span><span class="q-a-result">Having sex doesn’t cause the coil to fall out. But if you are experiencing any unusual pain that you think is related to your coil, contact your GP or Contraception and Sexual Health Service as soon as possible. <a href="" class="find-out">Find out about the coil here</a></span></p>
-			</div>
-		</div>
-	</div>
-	</div>
-</section>-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
